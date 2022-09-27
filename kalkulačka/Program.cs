c8e8ca3cfadce9a1c2d7cc9fba306394e67a3d0e
Rašín Adam <Rasin.Ad.2022@skola.ssps.cz>
2022-09-27 11:28:42 +0200

    first commit

---
+++ kalkulačka/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalkulačka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int cislo1 = int.Parse(Console.ReadLine());
+            int cislo = int.Parse(Console.ReadLine());
+            string operace = Console.ReadLine();
+            double vysledek = 0;
+
+            if (operace == "+")
+
+                vysledek = cislo1 + cislo;
+
+            else if (operace == "-")
+
+                vysledek = cislo1 - cislo;
+
+            else if (operace == "/")
+
+                vysledek = cislo1 / cislo;
+
+            else if (operace == "*")
+
+                vysledek = cislo1 * cislo;
+
+            else if (operace == "%")
+                vysledek = cislo1 % cislo;
+
+            else
+
+                Console.WriteLine("Chyba, špatný znak");
+
+
+
+
+
+
+        
+        
+
+
+
+            Console.WriteLine("Výsledek: " + vysledek);
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
